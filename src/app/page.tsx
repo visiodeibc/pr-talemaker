fddba6b18915bfdb50d26c3c34937e4e83d49ab3
Jake <ljh95714@gmail.com>
2024-04-12 00:09:22 +0900

    change main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,236 +1,48 @@
 "use client";
 import * as React from "react";
-import { useEffect, useRef, useState } from "react";
 import Box from "@mui/material/Box";
-import { Button, CircularProgress, TextField } from "@mui/material";
-import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-
-import WaveSurfer from "wavesurfer.js";
-import { transcribeSpeech } from "@/service/serviceGoogle";
-import { audioBlobToBase64 } from "@/util";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import { useRouter } from "next/navigation";
 
 export default function HomePage() {
-  const waveformRef = useRef(null);
-  const [waveSurfer, setWaveSurfer] = useState<WaveSurfer | null>(null);
-  const [audioFile, setAudioFile] = useState<Blob | null>(null);
-  const [transcription, setTranscription] = useState<string>(
-    "Record Something...."
-  );
-  const [transcribing, setTranscribing] = useState<boolean>(false);
+  const router = useRouter();
 
-  const [recording, setRecording] = useState<boolean>(false);
-  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(
-    null
-  );
-
-  const transcribe = async () => {
-    if (audioFile) {
-      setTranscribing(true);
-      const base64Audio = await audioBlobToBase64(audioFile);
-      try {
-        const result = await transcribeSpeech(base64Audio);
-        if (result) {
-          setTranscription(result);
-        } else {
-          console.log("No transcription results in the API response:");
-          setTranscription("No transcription available");
-        }
-      } catch (e) {
-        console.log(e);
-      } finally {
-        setTranscribing(false);
-      }
-    }
-  };
-
-  const play = () => {
-    !!waveSurfer && waveSurfer.play();
-  };
-
-  const handleRecord = async () => {
-    if (!recording) {
-      if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-        console.log("getUserMedia not supported on your browser!");
-        return;
-      }
-      const stream = await navigator.mediaDevices.getUserMedia({
-        audio: true,
-      });
-      const newMediaRecorder = new MediaRecorder(stream);
-      let chunks: BlobPart[] = [];
-
-      newMediaRecorder.ondataavailable = function (e: BlobEvent) {
-        chunks.push(e.data);
-      };
-
-      newMediaRecorder.onstop = function () {
-        const blob = new Blob(chunks, {
-          type: "audio/ogg; codecs=opus",
-        });
-        setAudioFile(blob);
-        chunks = [];
-      };
-      newMediaRecorder.start();
-      setMediaRecorder(newMediaRecorder);
-    } else {
-      if (mediaRecorder) {
-        mediaRecorder.stop();
-      }
-    }
-    setRecording(!recording);
-  };
-
-  useEffect(() => {
-    if (waveSurfer) {
-      waveSurfer.destroy();
-    }
-    if (audioFile) {
-      const wave = WaveSurfer.create({
-        container: waveformRef.current || "",
-        waveColor: "violet",
-        progressColor: "purple",
-      });
-      const audioUrl = URL.createObjectURL(audioFile);
-      if (wave) {
-        wave.load(audioUrl);
-        wave.on("ready", function () {
-          URL.revokeObjectURL(audioUrl);
-        });
-      }
-      setWaveSurfer(wave);
-    }
-
-    return () => {
-      if (waveSurfer) {
-        waveSurfer.destroy();
-      }
-    };
-  }, [audioFile]);
+  // Define your tiles here
+  const tiles = [
+    { title: "The Adventures of React Man", color: "#ffb3b3" },
+    { title: "The Mysterious Case of Redux Island", color: "#b3ffb3" },
+    { title: "The Journey to TypeScript Land", color: "#b3b3ff" },
+    { title: "The Magical World of CSS Wizards", color: "#ffffb3" },
+    { title: "The Enigma of API Integration", color: "#b3ffff" },
+    { title: "The Quest for Bug-Free Code", color: "#ffb3ff" },
+    { title: "+", color: "#qab3df" },
+  ];
 
   return (
-    <>
-      <Box
-        sx={{
-          padding: "20px",
-          bgcolor: "background.paper",
-          borderRadius: "12px",
-          boxShadow: 2,
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <Box sx={{ fontSize: "h6.fontSize", fontWeight: "bold" }}>
-            Recording
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              margin: "20px",
-            }}
-          >
-            {audioFile && (
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  padding: "20px",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                }}
-              >
-                <Box
-                  ref={waveformRef}
-                  style={{
-                    width: "200px",
-                    marginRight: "20px",
-                  }}
-                />
-                <Button
-                  variant="contained"
-                  sx={{
-                    borderRadius: "14px",
-                    height: "40px",
-                  }}
-                  color={"secondary"}
-                  onClick={play}
-                  disabled={!audioFile}
-                >
-                  <PlayArrowIcon />
-                </Button>
-              </Box>
-            )}
-            <Button
-              variant="contained"
-              sx={{ borderRadius: "14px", height: "40px" }}
-              color={recording ? "error" : "primary"}
-              onClick={handleRecord}
-            >
-              {recording && (
-                <CircularProgress
-                  color={"inherit"}
-                  size={20}
-                  sx={{ marginRight: "5px" }}
-                />
-              )}
-              {recording ? "Stop" : "Record"}
-            </Button>
-          </Box>
-        </Box>
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          padding: "20px",
-          marginTop: "20px",
-          borderRadius: "12px",
-          bgcolor: "background.paper",
-          boxShadow: 2,
-        }}
-      >
-        <Box sx={{ fontSize: "h6.fontSize", fontWeight: "bold" }}>
-          Transcription
-          {audioFile && (
-            <Button
-              variant="contained"
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        {tiles.map((tile, index) => (
+          <Grid item xs={4} key={index}>
+            <Paper
               sx={{
-                borderRadius: "14px",
-                height: "40px",
-                marginLeft: "10px",
+                height: 140,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: tile.color,
+                cursor: "pointer",
               }}
-              color={"primary"}
-              disabled={transcribing}
-              onClick={transcribe}
+              onClick={() => {
+                router.push("/story");
+              }}
             >
-              {transcribing && (
-                <CircularProgress
-                  color={"inherit"}
-                  size={20}
-                  sx={{ marginRight: "5px" }}
-                />
-              )}
-
-              {"Transcribe"}
-            </Button>
-          )}
-        </Box>
-        <TextField
-          sx={{ marginTop: "20px" }}
-          id="outlined-multiline-static"
-          label="transcribed text"
-          multiline
-          rows={4}
-          value={transcription}
-          variant="filled"
-          aria-readonly
-        />
-      </Box>
-    </>
+              <Typography variant="h6">{tile.title}</Typography>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 }
---
+++ src/app/story/page.tsx
@@ -0,0 +1,236 @@
+"use client";
+import * as React from "react";
+import { useEffect, useRef, useState } from "react";
+import Box from "@mui/material/Box";
+import { Button, CircularProgress, TextField } from "@mui/material";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+
+import WaveSurfer from "wavesurfer.js";
+import { transcribeSpeech } from "@/service/serviceGoogle";
+import { audioBlobToBase64 } from "@/util";
+
+export default function HomePage() {
+  const waveformRef = useRef(null);
+  const [waveSurfer, setWaveSurfer] = useState<WaveSurfer | null>(null);
+  const [audioFile, setAudioFile] = useState<Blob | null>(null);
+  const [transcription, setTranscription] = useState<string>(
+    "Record Something...."
+  );
+  const [transcribing, setTranscribing] = useState<boolean>(false);
+
+  const [recording, setRecording] = useState<boolean>(false);
+  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(
+    null
+  );
+
+  const transcribe = async () => {
+    if (audioFile) {
+      setTranscribing(true);
+      const base64Audio = await audioBlobToBase64(audioFile);
+      try {
+        const result = await transcribeSpeech(base64Audio);
+        if (result) {
+          setTranscription(result);
+        } else {
+          console.log("No transcription results in the API response:");
+          setTranscription("No transcription available");
+        }
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setTranscribing(false);
+      }
+    }
+  };
+
+  const play = () => {
+    !!waveSurfer && waveSurfer.play();
+  };
+
+  const handleRecord = async () => {
+    if (!recording) {
+      if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+        console.log("getUserMedia not supported on your browser!");
+        return;
+      }
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+      });
+      const newMediaRecorder = new MediaRecorder(stream);
+      let chunks: BlobPart[] = [];
+
+      newMediaRecorder.ondataavailable = function (e: BlobEvent) {
+        chunks.push(e.data);
+      };
+
+      newMediaRecorder.onstop = function () {
+        const blob = new Blob(chunks, {
+          type: "audio/ogg; codecs=opus",
+        });
+        setAudioFile(blob);
+        chunks = [];
+      };
+      newMediaRecorder.start();
+      setMediaRecorder(newMediaRecorder);
+    } else {
+      if (mediaRecorder) {
+        mediaRecorder.stop();
+      }
+    }
+    setRecording(!recording);
+  };
+
+  useEffect(() => {
+    if (waveSurfer) {
+      waveSurfer.destroy();
+    }
+    if (audioFile) {
+      const wave = WaveSurfer.create({
+        container: waveformRef.current || "",
+        waveColor: "violet",
+        progressColor: "purple",
+      });
+      const audioUrl = URL.createObjectURL(audioFile);
+      if (wave) {
+        wave.load(audioUrl);
+        wave.on("ready", function () {
+          URL.revokeObjectURL(audioUrl);
+        });
+      }
+      setWaveSurfer(wave);
+    }
+
+    return () => {
+      if (waveSurfer) {
+        waveSurfer.destroy();
+      }
+    };
+  }, [audioFile]);
+
+  return (
+    <>
+      <Box
+        sx={{
+          padding: "20px",
+          bgcolor: "background.paper",
+          borderRadius: "12px",
+          boxShadow: 2,
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Box sx={{ fontSize: "h6.fontSize", fontWeight: "bold" }}>
+            Recording
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              margin: "20px",
+            }}
+          >
+            {audioFile && (
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  padding: "20px",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Box
+                  ref={waveformRef}
+                  style={{
+                    width: "200px",
+                    marginRight: "20px",
+                  }}
+                />
+                <Button
+                  variant="contained"
+                  sx={{
+                    borderRadius: "14px",
+                    height: "40px",
+                  }}
+                  color={"secondary"}
+                  onClick={play}
+                  disabled={!audioFile}
+                >
+                  <PlayArrowIcon />
+                </Button>
+              </Box>
+            )}
+            <Button
+              variant="contained"
+              sx={{ borderRadius: "14px", height: "40px" }}
+              color={recording ? "error" : "primary"}
+              onClick={handleRecord}
+            >
+              {recording && (
+                <CircularProgress
+                  color={"inherit"}
+                  size={20}
+                  sx={{ marginRight: "5px" }}
+                />
+              )}
+              {recording ? "Stop" : "Record"}
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          padding: "20px",
+          marginTop: "20px",
+          borderRadius: "12px",
+          bgcolor: "background.paper",
+          boxShadow: 2,
+        }}
+      >
+        <Box sx={{ fontSize: "h6.fontSize", fontWeight: "bold" }}>
+          Transcription
+          {audioFile && (
+            <Button
+              variant="contained"
+              sx={{
+                borderRadius: "14px",
+                height: "40px",
+                marginLeft: "10px",
+              }}
+              color={"primary"}
+              disabled={transcribing}
+              onClick={transcribe}
+            >
+              {transcribing && (
+                <CircularProgress
+                  color={"inherit"}
+                  size={20}
+                  sx={{ marginRight: "5px" }}
+                />
+              )}
+
+              {"Transcribe"}
+            </Button>
+          )}
+        </Box>
+        <TextField
+          sx={{ marginTop: "20px" }}
+          id="outlined-multiline-static"
+          label="transcribed text"
+          multiline
+          rows={4}
+          value={transcription}
+          variant="filled"
+          aria-readonly
+        />
+      </Box>
+    </>
+  );
+}
