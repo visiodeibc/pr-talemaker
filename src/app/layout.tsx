92b77bafd26f132d751432e225d85eacf49233e0
Jake <ljh95714@gmail.com>
2024-04-11 23:54:59 +0900

    color theme / layout change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,12 @@
 import * as React from "react";
-import Link from "next/link";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import AudiotrackIcon from "@mui/icons-material/Audiotrack"; // import StarIcon from '@mui/icons-material/Star';
-// import ChecklistIcon from '@mui/icons-material/Checklist';
 import SettingsIcon from "@mui/icons-material/Settings";
-// import SupportIcon from '@mui/icons-material/Support';
-// import LogoutIcon from '@mui/icons-material/Logout';
 import ThemeRegistry from "@/components/ThemeRegistry/ThemeRegistry";
 
 export const metadata = {
@@ -45,7 +35,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body style={{ backgroundColor: "#C6DCBA" }}>
         <ThemeRegistry>
           <AppBar position="fixed" sx={{ zIndex: 2000 }}>
             <Toolbar sx={{ backgroundColor: "background.paper" }}>
@@ -61,54 +51,10 @@ export default function RootLayout({
               </Typography>
             </Toolbar>
           </AppBar>
-          {/* <Drawer
-            sx={{
-              width: DRAWER_WIDTH,
-              flexShrink: 0,
-              "& .MuiDrawer-paper": {
-                width: DRAWER_WIDTH,
-                boxSizing: "border-box",
-                top: ["48px", "56px", "64px"],
-                height: "auto",
-                bottom: 0,
-              },
-            }}
-            variant="permanent"
-            anchor="left"
-          >
-            <Divider />
-            <List>
-              {LINKS.map(({ text, href, icon: Icon }) => (
-                <ListItem key={href} disablePadding>
-                  <ListItemButton component={Link} href={href}>
-                    <ListItemIcon>
-                      <Icon />
-                    </ListItemIcon>
-                    <ListItemText primary={text} />
-                  </ListItemButton>
-                </ListItem>
-              ))}
-            </List>
-            <Divider sx={{ mt: "auto" }} />
-            <List>
-              {PLACEHOLDER_LINKS.map(({ text, icon: Icon }) => (
-                <ListItem key={text} disablePadding>
-                  <ListItemButton>
-                    <ListItemIcon>
-                      <Icon />
-                    </ListItemIcon>
-                    <ListItemText primary={text} />
-                  </ListItemButton>
-                </ListItem>
-              ))}
-            </List>
-          </Drawer> */}
           <Box
             component="main"
             sx={{
               flexGrow: 1,
-              bgcolor: "background.default",
-              // ml: `${DRAWER_WIDTH}px`,
               mt: ["48px", "56px", "64px"],
               p: 3,
             }}
--- src/components/ThemeRegistry/theme.ts
+++ src/components/ThemeRegistry/theme.ts
@@ -1,30 +1,47 @@
-import { Roboto } from 'next/font/google'
-import { createTheme } from '@mui/material/styles'
+import { Roboto } from "next/font/google";
+import { createTheme } from "@mui/material/styles";
 
 const roboto = Roboto({
-    weight: ['300', '400', '500', '700'],
-    subsets: ['latin'],
-    display: 'swap',
-})
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 const theme = createTheme({
-    palette: {
-        mode: 'light',
+  palette: {
+    primary: {
+      main: "#EFBC9B",
     },
-    typography: {
-        fontFamily: roboto.style.fontFamily,
+    secondary: {
+      main: "#FBF3D5",
     },
-    components: {
-        MuiAlert: {
-            styleOverrides: {
-                root: ({ ownerState }) => ({
-                    ...(ownerState.severity === 'info' && {
-                        backgroundColor: '#60a5fa',
-                    }),
-                }),
-            },
-        },
-    },
-})
 
-export default theme
+    error: {
+      main: "#red",
+    },
+    background: {
+      default: "#fff",
+      paper: "#D7E4C0",
+    },
+    text: {
+      primary: "#423731", // your desired color
+    },
+    mode: "light",
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+  components: {
+    MuiAlert: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...(ownerState.severity === "info" && {
+            backgroundColor: "#60a5fa",
+          }),
+        }),
+      },
+    },
+  },
+});
+
+export default theme;
