8fd6778552e87c70583007b892d8523748f18652
Jake <ljh95714@gmail.com>
2024-04-12 17:00:00 +0900

    update images

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,13 +1,13 @@
 // Define your tiles here
 export const books = [
   { index: 0, title: "Korea", color: "#ffb3b3", image: "/cover/korea.png" },
-  { index: 1, title: "Japan", color: "#b3ffb3", image: "/cover/korea.png" },
+  { index: 1, title: "Japan", color: "#b3ffb3", image: "/cover/japan.png" },
   { index: 2, title: "China", color: "#b3b3ff", image: "/cover/china.png" },
   {
     index: 3,
     title: "Singapore",
     color: "#ffffb3",
-    image: "/cover/korea.png",
+    image: "/cover/singapore.png",
   },
   { index: 4, title: "India", color: "#b3ffff", image: "/cover/india.png" },
   {
